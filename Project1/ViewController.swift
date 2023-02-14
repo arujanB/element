4704cd940860037091e24b249254c745efd67db2
arujanB <aruzhanboranbay02@gmail.com>
2023-02-14 13:29:10 +0600

    Row Line Interface(horizontal)

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -28,30 +28,6 @@ final class ViewController: UIViewController {
         return view
     }()
     
-    
-    
-    private lazy var myLabel: UILabel = {
-        var label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 70)
-        label.text = "I am rich"
-        label.textColor = .white
-        return label
-    }()
-    
-    private lazy var myDiamond: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "diamond")
-        return imageView
-    }()
-    
-    var randomNumber: UILabel = {
-        var random = UILabel()
-        random.text = "\(Int.random(in: 100..<1000))$"
-        random.font = UIFont.boldSystemFont(ofSize: 30)
-        random.textColor = .purple
-        return random
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -68,79 +44,35 @@ final class ViewController: UIViewController {
 private extension ViewController {
     func setUpView() {
         view.addSubview(redView)
-        redView.addSubview(greenView)
-        redView.addSubview(yellowView)
-        
-        view.addSubview(myLabel)
-        view.addSubview(myDiamond)
-        myDiamond.addSubview(randomNumber)
+        view.addSubview(greenView)
+        view.addSubview(yellowView)
         
     }
     
     func setUpConstrains() {
-        myLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.bottom.equalTo(myDiamond.snp.top).offset(-100)
-        }
-        
-        myDiamond.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.width.height.equalTo(200)
-        }
-        
-        randomNumber.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().dividedBy(2)
-        }
-        
-        
-        
-        
-        
         redView.snp.makeConstraints { make in
-            //this one we just create in CENTER
             make.center.equalToSuperview()
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.trailing.equalToSuperview()
             
-            //here we give SIZES (WIDTH, HEIGHT)      there all shown severel variants to give WIDTH and HEIGHT
-            make.width.equalToSuperview().multipliedBy(0.95)
-//            make.height.equalToSuperview().multipliedBy(0.95)
-//            make.width.equalTo(view.snp.width).multipliedBy(0.95)
-            make.height.equalTo(view.snp.height).multipliedBy(0.95)
-//            make.size.equalTo(view.snp.height).multipliedBy(0.95)
-            
-//            edges // edges - it is : leading, trailing, top, bottom
+            make.height.equalTo(view.safeAreaLayoutGuide.snp.height).dividedBy(3)
         }
         
         greenView.snp.makeConstraints { make in
-//            make.center.equalToSuperview()
-            
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.top.equalTo(redView.snp.bottom)
             make.leading.equalToSuperview()
-//            make.trailing.equalToSuperview()
-            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
-            
-//            make.height.equalToSuperview().multipliedBy(0.5)
-        }
-        yellowView.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.leading.equalTo(greenView.snp.trailing).offset(10)
             make.trailing.equalToSuperview()
-            make.bottom.equalToSuperview()
             
-            make.width.equalToSuperview().multipliedBy(0.5)
+            make.height.equalTo(view.safeAreaLayoutGuide.snp.height).dividedBy(3) //multiplied(0.33)
         }
         
-        
+        yellowView.snp.makeConstraints { make in
+            make.top.equalTo(greenView.snp.bottom)
+            make.leading.equalTo(view.snp.leading)
+            make.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
         
     }
 }
-
-
-/*
- make.top.equalTo(greenView.snp.bottom)
- make.leading.equalToSuperview()
- make.trailing.equalToSuperview()
- make.bottom.equalToSuperview()
- make.width.equalToSuperview().multipliedBy(0.5)
- 
- */
