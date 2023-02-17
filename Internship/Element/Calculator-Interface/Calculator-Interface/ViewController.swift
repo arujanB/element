01313066a17e0c7718508ef6ad75d72cd47a8859
arujanB <aruzhanboranbay02@gmail.com>
2023-02-17 18:11:35 +0600

    Table View Shorter Version

--- Internship/Element/Calculator-Interface/Calculator-Interface/ViewController.swift
+++ Internship/Element/Calculator-Interface/Calculator-Interface/ViewController.swift
@@ -22,272 +22,44 @@ final class ViewController: UIViewController {
         return label
     }()
     
+    private var myColor: UIColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
     
     private lazy var numbersBiggerViewDelete = UIView()
-    private lazy var buttonAC: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("AC", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var buttonSlash: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("+/-", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var buttonPersent: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("%", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var buttonDivided: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("/", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        
-        button.backgroundColor = .orange
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    
+    private lazy var buttonAC: UIButton = createButton(number: "AC", backgroundColor: myColor, titleColor: .gray)
+    private lazy var buttonSlash: UIButton = createButton(number: "+/-", backgroundColor: myColor, titleColor: .gray)
+    private lazy var buttonPersent: UIButton = createButton(number: "%", backgroundColor: myColor, titleColor: .gray)
+    private lazy var buttonDivided: UIButton = createButton(number: "/", backgroundColor: .orange, titleColor: .white)
     
     
     private lazy var numbersBiggerViewSeven = UIView()
-    private lazy var button7: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("7", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var button8: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("8", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var button9: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("9", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var buttonX: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("X", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        
-        button.backgroundColor = .orange
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    
+    private lazy var button7: UIButton = createButton(number: "7", backgroundColor: myColor, titleColor: .gray)
+    private lazy var button8: UIButton = createButton(number: "8", backgroundColor: myColor, titleColor: .gray)
+    private lazy var button9: UIButton = createButton(number: "9", backgroundColor: myColor, titleColor: .gray)
+    private lazy var buttonX: UIButton = createButton(number: "x", backgroundColor: .orange, titleColor: .white)
 
     
     private lazy var numbersBiggerViewFour = UIView()
-    private lazy var button4: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("4", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var button5: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("5", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var button6: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("6", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var buttonMinus: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("-", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        
-        button.backgroundColor = .orange
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-
+    private lazy var button4: UIButton = createButton(number: "4", backgroundColor: myColor, titleColor: .gray)
+    private lazy var button5: UIButton = createButton(number: "5", backgroundColor: myColor, titleColor: .gray)
+    private lazy var button6: UIButton = createButton(number: "6", backgroundColor: myColor, titleColor: .gray)
+    private lazy var buttonMinus: UIButton = createButton(number: "-", backgroundColor: .orange, titleColor: .white)
     
 
     private lazy var numbersBiggerViewOne = UIView()
-    private lazy var button1: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("1", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var button2: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("2", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var button3: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("3", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var buttonPlus: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("+", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        
-        button.backgroundColor = .orange
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
+    private lazy var button1: UIButton = createButton(number: "1", backgroundColor: myColor, titleColor: .gray)
+    private lazy var button2: UIButton = createButton(number: "2", backgroundColor: myColor, titleColor: .gray)
+    private lazy var button3: UIButton = createButton(number: "3", backgroundColor: myColor, titleColor: .gray)
+    private lazy var buttonPlus: UIButton = createButton(number: "+", backgroundColor: .orange, titleColor: .white)
 
     
     
     private lazy var numbersBiggerView0 = UIView()
     private lazy var numbersBiggerViewDot = UIView()
-    private lazy var button0: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("0", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var buttonDot: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle(".", for: .normal)
-        button.setTitleColor(.gray, for: .normal)
-        
-        button.backgroundColor = UIColor(red: 233, green: 232, blue: 232, alpha: 1)
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
-    private lazy var buttonEqual: UIButton = {
-        var button = UIButton(type: .system)
-        button.setTitle("=", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        
-        button.backgroundColor = .orange
-        
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
-        button.layer.borderColor = UIColor.gray.cgColor
-        button.layer.borderWidth = 1
-        
-        return button
-    }()
+    private lazy var button0: UIButton = createButton(number: "0", backgroundColor: UIColor(red: 233, green: 232, blue: 232, alpha: 1), titleColor: .gray)
+    
+    private lazy var buttonDot: UIButton = createButton(number: ".", backgroundColor: UIColor(red: 233, green: 232, blue: 232, alpha: 1), titleColor: .gray)
+    
+    private lazy var buttonEqual: UIButton = createButton(number: "=", backgroundColor: .orange, titleColor: .white)
     
     
     override func viewDidLoad() {
@@ -295,13 +67,13 @@ final class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = .black
         
-        biggerViewNumbers.backgroundColor = .red
-        
         setUpView()
         setUPConstrains()
     }
-
+    
 }
+
+//MARK: - SetUpView and SetUPConstrains
 
 private extension ViewController {
     func setUpView() {
@@ -309,7 +81,7 @@ private extension ViewController {
         view.addSubview(biggerViewNumbers)
         
         biggerViewBlack.addSubview(myLabel)
-        
+    
         
         
         biggerViewNumbers.addSubview(numbersBiggerViewDelete)
@@ -362,7 +134,6 @@ private extension ViewController {
             make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
             
             make.height.equalTo(view.safeAreaLayoutGuide.snp.height).multipliedBy(0.25)
-            
         }
         
         biggerViewNumbers.snp.makeConstraints { make in
@@ -370,7 +141,6 @@ private extension ViewController {
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
             make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
             make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
-            
         }
         
         myLabel.snp.makeConstraints { make in
@@ -384,27 +154,7 @@ private extension ViewController {
             make.height.equalToSuperview().dividedBy(5)
             make.width.equalToSuperview()
         }
-        
-        buttonAC.snp.makeConstraints { make in
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        buttonSlash.snp.makeConstraints { make in
-            make.leading.equalTo(buttonAC.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        buttonPersent.snp.makeConstraints { make in
-            make.leading.equalTo(buttonSlash.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        buttonDivided.snp.makeConstraints { make in
-            make.leading.equalTo(buttonPersent.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        
+        setUpButton(firstBtn: buttonAC, secondBtn: buttonSlash, thirdBtn: buttonPersent, forthBtn: buttonDivided)
         
         
         numbersBiggerViewSeven.snp.makeConstraints { make in
@@ -413,26 +163,7 @@ private extension ViewController {
             
             make.top.equalTo(numbersBiggerViewDelete.snp.bottom)
         }
-        
-        button7.snp.makeConstraints { make in
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        button8.snp.makeConstraints { make in
-            make.leading.equalTo(button7.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        button9.snp.makeConstraints { make in
-            make.leading.equalTo(button8.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        buttonX.snp.makeConstraints { make in
-            make.leading.equalTo(button9.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
+        setUpButton(firstBtn: button7, secondBtn: button8, thirdBtn: button9, forthBtn: buttonX)
         
         
         numbersBiggerViewFour.snp.makeConstraints { make in
@@ -441,26 +172,7 @@ private extension ViewController {
             
             make.top.equalTo(numbersBiggerViewSeven.snp.bottom)
         }
-        
-        button4.snp.makeConstraints { make in
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        button5.snp.makeConstraints { make in
-            make.leading.equalTo(button4.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        button6.snp.makeConstraints { make in
-            make.leading.equalTo(button5.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        buttonMinus.snp.makeConstraints { make in
-            make.leading.equalTo(button6.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
+        setUpButton(firstBtn: button4, secondBtn: button5, thirdBtn: button6, forthBtn: buttonMinus)
         
         
         numbersBiggerViewOne.snp.makeConstraints { make in
@@ -469,26 +181,7 @@ private extension ViewController {
             
             make.top.equalTo(numbersBiggerViewFour.snp.bottom)
         }
-        
-        button1.snp.makeConstraints { make in
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        button2.snp.makeConstraints { make in
-            make.leading.equalTo(button1.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        button3.snp.makeConstraints { make in
-            make.leading.equalTo(button2.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
-        buttonPlus.snp.makeConstraints { make in
-            make.leading.equalTo(button3.snp.trailing)
-            make.width.equalToSuperview().dividedBy(4)
-            make.height.equalToSuperview()
-        }
+        setUpButton(firstBtn: button1, secondBtn: button2, thirdBtn: button3, forthBtn: buttonPlus)
         
         
         
@@ -517,7 +210,49 @@ private extension ViewController {
             make.width.equalToSuperview().dividedBy(2)
             make.height.equalToSuperview()
         }
+    }
+    
+}
+
+//MARK: - Function menthod of the Buttons
+extension ViewController {
+    func createButton(number: String, backgroundColor color: UIColor, titleColor: UIColor) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(number, for: .normal)
+        button.setTitleColor(titleColor, for: .normal)
+            
+        button.backgroundColor = color
         
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        button.layer.borderColor = UIColor.gray.cgColor
+        button.layer.borderWidth = 1
+//        button.layer.cornerRadius = 20
+        return button
+    }
+}
+
+//MARK: - SetUpButton Constrains
+extension ViewController {
+    func setUpButton(firstBtn: UIButton, secondBtn: UIButton, thirdBtn: UIButton, forthBtn: UIButton) {
+        firstBtn.snp.makeConstraints { make in
+            make.width.equalToSuperview().dividedBy(4)
+            make.height.equalToSuperview()
+        }
+        secondBtn.snp.makeConstraints { make in
+            make.leading.equalTo(firstBtn.snp.trailing)
+            make.width.equalToSuperview().dividedBy(4)
+            make.height.equalToSuperview()
+        }
+        thirdBtn.snp.makeConstraints { make in
+            make.leading.equalTo(secondBtn.snp.trailing)
+            make.width.equalToSuperview().dividedBy(4)
+            make.height.equalToSuperview()
+        }
+        forthBtn.snp.makeConstraints { make in
+            make.leading.equalTo(thirdBtn.snp.trailing)
+            make.width.equalToSuperview().dividedBy(4)
+            make.height.equalToSuperview()
+        }
     }
     
 }
